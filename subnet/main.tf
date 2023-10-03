4f424500ba54d262bb74658a1e999dab569de9b8
Anil Gencdogus <agencdogus@ziyotek.net>
2023-10-02 21:31:34 -0400

    updated with vpc and subnet

--- subnet/main.tf
+++ subnet/main.tf
@@ -6,4 +6,32 @@ terraform {
       name = "infra-subnet"
     }
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc_id" {
+  default = "vpc-0bb08df1093ea4c4f"
+}
+
+data "aws_vpc" "main" {
+  id = var.vpc_id
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = data.aws_vpc.main.id
+  cidr_block = cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
+
+  tags = {
+    Name = "Main"
+  }
 }
